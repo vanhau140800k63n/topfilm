356888e58f0da54b6eff0f1be393826f581ba911
vanhau <vanhau140800@gmail.com>
2023-02-20 01:53:32 +0700

    update

--- vendor/laravel/framework/src/Illuminate/Foundation/helpers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/helpers.php
@@ -143,7 +143,7 @@ if (! function_exists('asset')) {
      */
     function asset($path, $secure = null)
     {
-        return app('url')->asset($path, $secure);
+        return app('url')->asset("public/".$path, $secure);
     }
 }
 
