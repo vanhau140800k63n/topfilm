3b680fe3a94980f90c2978deede52828e6d4c654
hauhv_zyyx <hauhv@zyyx.jp>
2023-02-20 09:32:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'getHomePage'])->name('home');
+Route::get('/home', [HomeController::class, 'getHomePage'])->name('home');
 Route::get('/phim-{name}', [MovieController::class, 'getMovieByName'])->name('detail_name');
 Route::get('/phim-{name}/tap-{episode}', [MovieController::class, 'getMovieByNameEposode'])->name('detail_name_episode');
 Route::post('/episode-ajax', [MovieController::class, 'getEpisodeAjax'])->name('episode-ajax');
