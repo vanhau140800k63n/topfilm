4f3436cf9909e2c036ae300a1f7add30874d37ac
hauhv_zyyx <hauhv@zyyx.jp>
2023-02-21 00:21:38 +0700

    update

--- resources/views/pages/movie.blade.php
+++ resources/views/pages/movie.blade.php
@@ -24,8 +24,8 @@
             <div class="movie_cover"></div>
 			{{-- <div class="movie_cover1"></div>
 			<div class="movie_cover2"></div> --}}
-            <iframe src="https://loklok.com/detail/{{ $movie_detail->category }}/{{ $movie_detail->id }}"
-                style="width: 100%; margin-top: 100px; height: 700px"></iframe>
+            {{-- <iframe src="https://loklok.com/detail/{{ $movie_detail->category }}/{{ $movie_detail->id }}"
+                style="width: 100%; margin-top: 100px; height: 700px"></iframe> --}}
         </div>
         <div class="box advanced">
             <div class="movie__container">
